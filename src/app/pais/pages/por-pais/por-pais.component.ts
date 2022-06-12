8a26e61d31cb4be45c34d5f5d1c609f7f5d3ac39
Agustín Casado <agu.e.casado@gmail.com>
2022-06-12 02:24:09 -0300

    llenar tabla paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -14,15 +16,18 @@ export class PorPaisComponent {
   termino: string = "";
   hayError: boolean = false;
 
+  paises: Country[] = [];
+
   buscar(): void {
     this.hayError = false;
     console.log(this.termino);
     
     this.paisService.buscarPais( this.termino )
       .subscribe( paises => {
-        console.log(paises);
+        this.paises = paises;
       }, ( err ) => {
         this.hayError = true;
+        this.paises = [];
       });
   }
 
