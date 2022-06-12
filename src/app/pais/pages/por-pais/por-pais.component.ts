3f88c6ef689a83ef68ad361f9b529fe439bfdfe0
Agustín Casado <agu.e.casado@gmail.com>
2022-06-12 01:24:17 -0300

    manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -12,13 +12,17 @@ export class PorPaisComponent {
   constructor(private paisService:PaisService) { }
 
   termino: string = "";
+  hayError: boolean = false;
 
   buscar(): void {
+    this.hayError = false;
     console.log(this.termino);
     
     this.paisService.buscarPais( this.termino )
       .subscribe( resp => {
         console.log(resp);
+      }, ( err ) => {
+        this.hayError = true;
       });
   }
 
